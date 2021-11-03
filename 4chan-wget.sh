2597d78e3c472bd5c311d70db0342535c3280847
crusader290 <59276425+crusader290@users.noreply.github.com>
2021-11-03 15:28:11 +1000

    Update 4chan-wget.sh

--- 4chan-wget.sh
+++ 4chan-wget.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Enter Thread URL"
 read thread
-wget -P pictures -nd -r -l 1 -H -D i.4cdn.org -A png,gif,jpg,jpeg,webm -R '?????????????s' $thread
+wget -P pictures -nd -r -l 1 -H -D i.4cdn.org -A png,gif,jpg,jpeg,webm -R '?????????????s.*' $thread
